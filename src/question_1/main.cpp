6ca3934ec910135d4356bc84e766d4ad7e8c1315
babakAcc <135079991+babakAcc@users.noreply.github.com>
2023-06-28 20:21:33 +0000

    the loop and condition for validation was added

--- src/question_1/main.cpp
+++ src/question_1/main.cpp
@@ -2,14 +2,33 @@
 #include<iostream>
 
 using std::cout; using std::cin;
+using std::string;
 
 
 int main()
 {
     int number;
+    char keep_going;
 
-    cout<<"Enter a number: ";
-    cin>>number;
-    cout<<"Factorial of "<<number<< " is: "<<get_factorial_sequence(number)<<"\n";
+
+    do
+        {
+            cout<<"Enter a number: ";
+            cin>>number;
+                if(number > 10 || number < 1)
+                    {
+                        cout<<"Please enter a number between 1 and 10\n";
+                    }
+                    else 
+                    {
+                    cout<<"Factorial of "<<number<< " is: "<<get_factorial_sequence(number)<<"\n";
+                    }
+
+                    cout<<"Do you want to enter another number?(y / n): ";
+                    cin>>keep_going; 
+
+        } while (keep_going == 'y' || keep_going == 'Y');
+    
+
     return 0;
 }
